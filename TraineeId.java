6f0db1ba7f748dd6006f354b31583da05c33bf75
Jalal Nofia <146335072+JalalNofia@users.noreply.github.com>
2024-05-05 15:17:12 +0530

    Add files via upload

---
+++ TraineeId.java
@@ -0,0 +1,11 @@
+package com.java.code;
+
+public class TraineeId {
+public static void main(String[] args) {
+		
+	
+  System.out.println("My name is Nofia Jalal");
+  System.out.println("My designation is Java developer");
+  
+}
+}
