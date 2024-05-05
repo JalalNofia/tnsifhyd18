4c3a1583fd2e2c41c41bfcf7e0256394acce9786
Jalal Nofia <146335072+JalalNofia@users.noreply.github.com>
2024-05-05 15:36:19 +0530

    Add files via upload

---
+++ Float.java
@@ -0,0 +1,11 @@
+package com.java.code;
+
+public class Float {
+	
+		public static void main(String[] args) {
+			int quantity=5;
+			float totalCost = 10 * quantity; // error on this line
+			System.out.println(totalCost);
+		}
+	}
+
