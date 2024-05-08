7399e6c19e32316cc63c5c409e2aa88803629630
Jalal Nofia <146335072+JalalNofia@users.noreply.github.com>
2024-05-08 16:57:41 +0530

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,14 @@
+package com.java.practice;
+
+public class Bitwise {
+public static void main(String[] args) {
+	int a=20;
+	int b=30;
+	System.out.println(a<<2);
+	System.out.println(b<<2);
+	System.out.println(a>>2);
+	System.out.println(b>>2);
+	System.out.println(a & b);
+	System.out.println(a | b);
+}
+}
