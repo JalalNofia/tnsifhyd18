aa760619366f2d6663408a1f34ef93b472abd12d
Jalal Nofia <146335072+JalalNofia@users.noreply.github.com>
2024-05-09 17:11:33 +0530

    Add files via upload

---
+++ CircAreaMain.java
@@ -0,0 +1,13 @@
+package com.java.practice;
+
+import java.util.Scanner;
+
+public class CircAreaMain {
+public static void main(String[] args) {
+	
+	
+	CircArea a1=new CircArea();
+	a1.display();
+	System.out.println("the area of circle is: "+a1.display());
+}
+}
